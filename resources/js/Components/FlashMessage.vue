a0139bc2b9fad47d7c2a2baf5916be989b2cfb54
demonyka <cweranit@gmail.com>
2024-04-18 03:16:23 +0530

    flash mobile

--- resources/js/Components/FlashMessage.vue
+++ resources/js/Components/FlashMessage.vue
@@ -26,22 +26,5 @@ export default {
 </template>
 
 <style scoped>
-    .flash {
-        position: fixed;
-        padding: 30px;
-        right: 50%;
-        transform: translateX(50%);
-        bottom: 20px;
-        background-color: var(--gray2);
-        border-radius: 20px;
-        color: var(--white);
-        max-width: 40vw;
-        text-align: center;
-    }
-    .flash.success {
-        background-color: var(--green1);
-    }
-    .flash.error {
-        background-color: var(--red);
-    }
+
 </style>
